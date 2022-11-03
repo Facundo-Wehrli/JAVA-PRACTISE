04d41ce55eb0ed2f0520aa1a3f5e7ab82c907054
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-03 20:44:37 -0300

    ignore

---
+++ PRACTICO desarrollo de metodos/src/apipi.java
@@ -0,0 +1,20 @@
+public class apipi {
+    public static void baffle(String blimp) { // 8
+        System.out.println(blimp); // 9
+        zippo("ping", -5); // 10
+    }
+
+    public static void zippo(String quince, int flag) { // 3,11
+        if (flag < 0) { // 4,12
+            System.out.println(quince + "zoop"); // 13
+        } else { // 5
+            System.out.println("ik"); // 6
+            baffle(quince); // 7
+            System.out.println("bo-wa-ha-ha"); // 14
+        }
+    }
+
+    public static void main(String[] args) { // 1
+        zippo("rattle", 13);// 2
+    }
+}
