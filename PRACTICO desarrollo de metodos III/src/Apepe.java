b36f21baba9bb265839806def26f31635e83f95e
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-03 08:12:11 -0300

    listo

--- PRACTICO desarrollo de metodos III/src/Apepe.java
+++ PRACTICO desarrollo de metodos III/src/Apepe.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-// Ejercicio 1.
-// Crear una aplicación java de consola que contengan lo siguientes:
-// 1. Crear un método que solicite datos personales a los usuarios ejemplo: nombre,
-// apellido y la edad, y sean ingresado por teclado.
-// 2. Crear otro método que imprima los datos de una persona ingresados por teclado e
-// indicar si es mayor o menor de edad.
-// 3. Crear otro método que almacene los datos ingresados por el usuario en un array,
-// puede ser unidimensional y por cada usuario se puede guardar la info concatenada,
-// ejemplo nombre + apellido + edad.
-// 4. Crear un método que imprima la lista final de usuarios ingresados.
 public class Apepe {
     // 1. Crear un método que solicite datos personales a los usuarios ejemplo:
     // nombre,
@@ -49,37 +39,36 @@ public class Apepe {
         } else
             System.out.println("error intente denuevo");
 
-        String usuario = "nombre :" + nombre + " apellido :" + apellido + " edad :" + edad +"("+mayorOmenor+")";
+        String usuario = "nombre :" + nombre + " apellido :" + apellido + " edad :" + edad + "(" + mayorOmenor + ")";
         obj.close();
         return usuario;
     }
 
+    // 3. Crear otro método que almacene los datos ingresados por el usuario en un
+    // array,
+    // puede ser unidimensional y por cada usuario se puede guardar la info
+    // concatenada,
+    // ejemplo nombre + apellido + edad.
 
-
-    // 3. Crear otro método que almacene los datos ingresados por el usuario en un array,
-// puede ser unidimensional y por cada usuario se puede guardar la info concatenada,
-// ejemplo nombre + apellido + edad.
-
-        public void UsuarioEnArray(){
-            Scanner obj = new Scanner(System.in);
-            System.out.println("ingrese su nombre");
-            String nombre = obj.nextLine();
-            System.out.println("ingrese su apellido");
-            String apellido = obj.nextLine();
-            System.out.println("ingrese su edad");
-            int edad = obj.nextInt();
-            String[] usuario = {"nombre :" + nombre + " apellido :" + apellido + " edad :" + edad};
-            obj.close();
-
-
-        }
-
-
+    public void UsuarioEnArray() {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre = obj.nextLine();
+        System.out.println("ingrese su apellido");
+        String apellido = obj.nextLine();
+        System.out.println("ingrese su edad");
+        int edad = obj.nextInt();
+        String[] usuario = { "nombre :" + nombre + " apellido :" + apellido + " edad :" + edad };
+        // el sgte paso es innecesario ya que no pide que lo imprima
+        System.out.println(Arrays.toString(usuario));
+        obj.close();
+    }
 
     public static void main(String[] args) {
         Apepe app = new Apepe();
-        // System.out.println(app.imprimirDatos_MayoriaEdad());
-       
+        System.out.println(app.imprimirDatos_MayoriaEdad());
+        app.UsuarioEnArray();
+
     }
 
 }
--- PRACTICO desarrollo de metodos III/src/App.java
+++ PRACTICO desarrollo de metodos III/src/App.java
@@ -1,33 +1,33 @@
-// Ejercicio 2.
-// ¿Cuál es el resultado del siguiente programa?
-public static void main(String[] args) {
-boolean flag1 = isHoopy(202);
-boolean flag2 = isFrabjuous(202);
-System.out.println(flag1);
-System.out.println(flag2);
-if (flag1 &amp;&amp; flag2) {
-System.out.println("ping!"");
-}
+// // Ejercicio 2.
+// // ¿Cuál es el resultado del siguiente programa?
+// public static void main(String[] args) {
+// boolean flag1 = isHoopy(202);
+// boolean flag2 = isFrabjuous(202);
+// System.out.println(flag1);
+// System.out.println(flag2);
+// if (flag1 &amp;&amp; flag2) {
+// System.out.println("ping!"");
+// }
 
-if (flag1 || flag2) {
-System.out.println("pong!");
-}
-}
-public static boolean isHoopy(int x) {
-boolean hoopyFlag;
-if (x % 2 == 0) {
-hoopyFlag = true;
-} else {
-hoopyFlag = false;
-}
-return hoopyFlag;
-}
-public static boolean isFrabjuous(int x) {
-boolean frabjuousFlag;
-if (x &gt; 0) {
-frabjuousFlag = true;
-} else {
-frabjuousFlag = false;
-}
-return frabjuousFlag;
-}
+// if (flag1 || flag2) {
+// System.out.println("pong!");
+// }
+// }
+// public static boolean isHoopy(int x) {
+// boolean hoopyFlag;
+// if (x % 2 == 0) {
+// hoopyFlag = true;
+// } else {
+// hoopyFlag = false;
+// }
+// return hoopyFlag;
+// }
+// public static boolean isFrabjuous(int x) {
+// boolean frabjuousFlag;
+// if (x &gt; 0) {
+// frabjuousFlag = true;
+// } else {
+// frabjuousFlag = false;
+// }
+// return frabjuousFlag;
+// }
