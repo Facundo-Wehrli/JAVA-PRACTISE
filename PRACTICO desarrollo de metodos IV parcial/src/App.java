c4875a99cdb0b9a08888762cf4cc5910fb433fc7
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-12 21:56:24 -0300

    tp4 practica parcial

---
+++ PRACTICO desarrollo de metodos IV parcial/src/App.java
@@ -0,0 +1,20 @@
+// Ejercicio 1.
+// Crear una aplicación java de consola que contenga lo siguiente:
+// 1.	Crear un método para mostrar la tabla de multiplicar de cualquier número ingresado por teclado.
+// Ejercicio 2.
+// Crear una aplicación java de consola que contenga lo siguiente: un método que simule el proceso de subir a un piso en un ascensor, donde el número de piso es ingresado por teclado.
+// Ejercicio 3.
+// Crear una aplicación java de consola que contenga lo siguiente:
+// 1.	Diseñe un método en el que se pida ingresar por teclado su género (M o F). 
+// 2.	Luego solicitar su nombre que le corresponde a un varón o mujer.
+// 3.	Imprimir por consola los datos ingresados por el usuario.
+// Ejercicio 4.
+// Crear una aplicación java de consola para un supermercado, donde se hace un 20% de descuento a los clientes cuya compra supere los $ 1.000 en las áreas de frutas, verduras y abarrotes. ¿Cuál será el total que pagará una persona por su compra?, se debe mostrar el nombre del cliente, producto, precio, cantidad, descuento y total a pagar. 
+// Crear los métodos que consideres necesarios para la resolución de esta aplicación.
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+    }
+}
