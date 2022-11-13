dfd0e67f991d97a03fbd67eecd67c14bf9d32662
Facundo Wehrli <wehrlifacundo9@gmail.com>
2022-11-13 17:36:29 -0300

    tp4 terminado

--- PRACTICO desarrollo de metodos IV parcial/src/App.java
+++ PRACTICO desarrollo de metodos IV parcial/src/App.java
@@ -14,59 +14,63 @@ import java.util.Scanner;
 // Crear una aplicación java de consola para un supermercado, donde se hace un 20% de descuento a los clientes cuya compra supere los $ 1.000 en las áreas de frutas, verduras y abarrotes. ¿Cuál será el total que pagará una persona por su compra?, se debe mostrar el nombre del cliente, producto, precio, cantidad, descuento y total a pagar. 
 // Crear los métodos que consideres necesarios para la resolución de esta aplicación.
 
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-    //   mostrarTabla();
-      subirPiso();
+  static Scanner obj = new Scanner(System.in);
+
+  public static void main(String[] args) throws Exception {
+    ingresarDatos();
+    // mostrarTabla();
+    // subirPiso();
+  }
+
+  // 1. Crear un método para mostrar la tabla de multiplicar de cualquier número
+  // ingresado por teclado.
+  // XXX ejercicio 1 terminado
+  public static void mostrarTabla() {
+    Scanner obj = new Scanner(System.in);
+    System.out.println("ingrese un número");
+    int num = obj.nextInt();
+    // Creating a variable that will not be modified when the for loop is executed.
+    int j = num;
+    for (int i = 0; i < 11; i++) {
+      num = j * i;
+      System.out.println(j + "multiplicado por " + i + " es = (" + num + ")");
     }
 
+  }
 
-// 1.	Crear un método para mostrar la tabla de multiplicar de cualquier número ingresado por teclado.
-//XXX ejercicio 1 terminado 
-    public static void mostrarTabla (){
-        Scanner obj = new Scanner(System.in);
-        System.out.println("ingrese un número");
-        int num = obj.nextInt();
-       // Creating a variable that will not be modified when the for loop is executed.
-        int j = num; 
-       for (int i = 0; i < 11; i++) {
-            num = j * i;
-            System.out.println(j+"multiplicado por "+i+ " es = ("+num+")");
-       }
+  // XXX ejercicio 2
 
-       obj.close();
+  // Ejercicio 2.
+  // Crear una aplicación java de consola que contenga lo siguiente: un método que
+  // simule el proceso de subir a un piso en un ascensor, donde el número de piso
+  // es ingresado por teclado.
+
+  public static void subirPiso() {
+    Scanner obj = new Scanner(System.in);
+    System.out.println("ingrese el número de piso al que quiere llegar");
+    int piso = obj.nextInt();
+    for (int i = 0; i <= piso; i++) {
+      if (i == 0) {
+        System.out.println("está en planta baja");
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+        i++; // acá lo aumenté para que después no ponga está en el piso 0 entonces empieza
+             // del 1
+      }
+      if (i == piso) {
+        System.out.println("hemos llegado al piso " + piso + " que nos pidió ya puede bajarse (musiquita de ascensor)");
+        break;
+      }
+      System.out.println("está en el piso " + i);
+      System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
     }
+  }
 
-//XXX ejercicio 2 
-
-// Ejercicio 2.
-// Crear una aplicación java de consola que contenga lo siguiente: un método que simule el proceso de subir a un piso en un ascensor, donde el número de piso es ingresado por teclado.
-
-    public static void subirPiso(){
-        Scanner obj = new Scanner(System.in);
-        System.out.println("ingrese el número de piso al que quiere llegar");
-        int piso = obj.nextInt();
-        for (int i = 0; i <= piso; i++) {
-          if (i == 0){
-            System.out.println("está en planta baja");
-            System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-            i++; // acá lo aumenté para que después no ponga está en el piso 0 entonces empieza del 1
-          } 
-          if (i == piso){
-            System.out.println("hemos llegado al piso "+piso+" que nos pidió ya puede bajarse (musiquita de ascensor)");
-            break;
-          }
-          System.out.println("está en el piso "+i); 
-          System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-        }
-
-
-
-        obj.close();
-    }
-
-
-
+  public static void ingresarDatos() {
+    System.out.println("ingrese su género M masculino, F femenino");
+    String genero = obj.nextLine();
+    System.out.println("ingrese su nombre");
+    String nombre = obj.nextLine();
+    System.out.println("Gracias " + nombre + " (" + genero + ") ! ha ingresado sus datos de manera exitosa ;)");
+  }
 }
